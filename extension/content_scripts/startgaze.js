d133d4a6d107c9a522bebc37e81d513b5ca02a1e
Rifaz Nahiyan <rifazn@protonmail.com>
2020-07-21 09:45:24 +0600

    webgazer was not ready because I did not start it.

--- extension/content_scripts/startgaze.js
+++ extension/content_scripts/startgaze.js
@@ -1,18 +1,9 @@
 window.applyKalmanFilter=true
 window.saveDataAcrossSessions=true
 
-function checkIfReady() {
-    if (webgazer.isReady()) {
-        webgazer.setRegression("ridge")
-            .begin()
-            .showPredictionPoints(true)
-    }
-    else {
-        console.log("WebGazer not ready")
-        setTimeout(checkIfReady, 100)
-    }
-}
-setTimeout(checkIfReady, 100)
+webgazer.setRegression("ridge")
+    .begin()
+    .showPredictionPoints(true)
 
 document.addEventListener("keydown", ev => {
     if (ev.ctrlKey && ev.key == ">") {
